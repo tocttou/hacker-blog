7c4c713970ab5c1496217dff31ad46a41c880795
Aman Ajay Varma <50282743+AmanAjayVarma@users.noreply.github.com>
2020-01-01 13:02:03 +0530

    Create main.js

---
+++ main.js
@@ -0,0 +1,12 @@
+{
+  "name": "amanajayvarma",
+  "short_name": "amanvarma:,
+  "icon": [{
+        "src": "/assets/icon.png"
+        "sizes": "192x192"
+        "type": "image/png"
+      }]
+  "start_url": "/index.html?homescreen=1",
+  "desplay": "standalone"
+  "gcm_sender_id": "715892663448"
+}
