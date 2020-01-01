9ce8cfc9c2e4dab0d8c30c9a3c0bb1a168d4b483
Aman Ajay Varma <50282743+AmanAjayVarma@users.noreply.github.com>
2020-01-01 13:21:34 +0530

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,13 @@
+self.addEventListener('push', function(event) {
+  console.log('[Service Worker] Push Received.');
+  console.log(`[Service Worker] Push had this data: "${event.data.text()}"`);
+
+  const title = 'AmanAjayVarm';
+  const options = {
+    body: 'You got a message.',
+    icon: 'images/icon.png',
+    badge: 'images/badge.png'
+  };
+
+  event.waitUntil(self.registration.showNotification(title, options));
+});
