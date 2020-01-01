57fdef80249730a2f8069e267aa2136dab0d082c
Aman Ajay Varma <50282743+AmanAjayVarma@users.noreply.github.com>
2020-01-01 13:17:48 +0530

    Create main.js

---
+++ main.js
@@ -0,0 +1,16 @@
+if ('serviceWorker' in navigator && 'PushManager' in window) {
+  console.log('Service Worker and Push is supported');
+
+  navigator.serviceWorker.register('sw.js')
+  .then(function(swReg) {
+    console.log('Service Worker is registered', swReg);
+
+    swRegistration = swReg;
+  })
+  .catch(function(error) {
+    console.error('Service Worker Error', error);
+  });
+} else {
+  console.warn('Push messaging is not supported');
+  pushButton.textContent = 'Push Not Supported';
+}
